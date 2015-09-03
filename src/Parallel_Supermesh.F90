dc47ddd6537e110fdce4e91fa7a8c57441ad8d0a
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2015-09-03 12:11:33 +0100

    One-way communication parallel supermeshing interface outline

---
+++ src/Parallel_Supermesh.F90
@@ -0,0 +1,83 @@
+module libsupermesh_parallel_supermesh
+
+  implicit none
+
+contains
+
+  subroutine parallel_supermesh(coords_a, enlist_a, coords_b, enlist_b, donor_cell_data, target_cell_data, intersection_calculation)
+    ! dim x nnodes_a
+    real, dimension(:, :), intent(in) :: coords_a
+    ! loc_a x nelements_a
+    integer, dimension(:, :), intent(in) :: enlist_a
+    ! dim x nnodes_b
+    real, dimension(:, :), intent(in) :: coords_b
+    ! loc_b x nelements_b
+    integer, dimension(:, :), intent(in) :: enlist_b
+    interface
+      subroutine donor_cell_data(eles, data, ndata, cell_data)
+        use iso_c_binding, only : c_ptr
+        implicit none
+        integer, dimension(:), intent(in) :: eles
+        type(c_ptr), intent(out) :: data
+        integer, intent(out) :: ndata
+        interface
+          subroutine cell_data(ele, data, ndata, ele_data, nele_data)
+            use iso_c_binding, only : c_ptr
+            implicit none
+            integer, intent(in) :: ele
+            type(c_ptr), intent(in) :: data
+            integer, intent(in) :: ndata
+            type(c_ptr), intent(out) :: ele_data
+            integer, intent(out) :: nele_data
+          end subroutine cell_data
+        end interface
+      end subroutine donor_cell_data
+      
+      subroutine target_cell_data(eles, data, ndata, cell_data)
+        use iso_c_binding, only : c_ptr
+        implicit none
+        integer, dimension(:), intent(in) :: eles
+        type(c_ptr), intent(out) :: data
+        integer, intent(out) :: ndata
+        interface
+          subroutine cell_data(ele, data, ndata, ele_data, nele_data)
+            use iso_c_binding, only : c_ptr
+            implicit none
+            integer, intent(in) :: ele
+            type(c_ptr), intent(in) :: data
+            integer, intent(in) :: ndata
+            type(c_ptr), intent(out) :: ele_data
+            integer, intent(out) :: nele_data
+          end subroutine cell_data
+        end interface
+      end subroutine target_cell_data
+
+      subroutine intersection_calculation(coords_c, ele_a, proc_a, ele_b, proc_b, ele_data_a, ele_ndata_a, ele_data_b, ele_ndata_b)
+        use iso_c_binding, only : c_ptr
+        implicit none
+        ! dim x (loc_c x nelements_c)
+        real, dimension(:, :), intent(in) :: coords_c
+        integer, intent(in) :: ele_a
+        integer, intent(in) :: proc_a
+        integer, intent(in) :: ele_b
+        integer, intent(in) :: proc_b
+        type(c_ptr), intent(in) :: ele_data_a
+        integer, intent(in) :: ele_ndata_a
+        type(c_ptr), intent(in) :: ele_data_b
+        integer, intent(in) :: ele_ndata_b
+      end subroutine intersection_calculation
+    end interface
+
+    ! 1. Communicate bounding box data for donor and target
+
+    ! 2. Use bounding box data to cull donor mesh
+
+    ! 3. Pack non-culled mesh data for communication, calling user specified data functions
+
+    ! 4. Communicate mesh and mesh data
+
+    ! 5. Supermesh and call user specified element unpack and calculation functions
+    
+  end subroutine parallel_supermesh
+
+end module libsupermesh_parallel_supermesh
