8ddb3e50944b03be89454955706fab3123926410
Dr. James R. Maddison <j.r.maddison@ed.ac.uk>
2016-04-12 16:26:31 +0100

    Further copyright information

--- src/tests/test_intersection_finder_completeness_1d.F90
+++ src/tests/test_intersection_finder_completeness_1d.F90
@@ -1,3 +1,55 @@
+!  Copyright (C) 2016 The University of Edinburgh
+!
+!  The file is part of libsupermesh
+!    
+!  This library is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation;
+!  version 2.1 of the License.
+!
+!  This library is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+!
+!  You should have received a copy of the GNU Lesser General Public
+!  License along with this library; if not, write to the Free Software
+!  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+
+! The following code is derived from
+! femtools/tests/test_intersection_finder_completeness.F90 in Fluidity git revision
+! 4e6c1d2b022df3a519cdec120fad28e60d1b08d9 (dated 2015-02-25)
+!
+! Fluidity copyright information (note that AUTHORS mentioned in the following
+! has been renamed to fluidity_AUTHORS):
+!
+!  Copyright (C) 2006 Imperial College London and others.
+!  
+!  Please see the AUTHORS file in the main source directory for a full list
+!  of copyright holders.
+!
+!  Prof. C Pain
+!  Applied Modelling and Computation Group
+!  Department of Earth Science and Engineering
+!  Imperial College London
+!
+!  amcgsoftware@imperial.ac.uk
+!  
+!  This library is free software; you can redistribute it and/or
+!  modify it under the terms of the GNU Lesser General Public
+!  License as published by the Free Software Foundation,
+!  version 2.1 of the License.
+!
+!  This library is distributed in the hope that it will be useful,
+!  but WITHOUT ANY WARRANTY; without even the implied warranty of
+!  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+!  Lesser General Public License for more details.
+!
+!  You should have received a copy of the GNU Lesser General Public
+!  License along with this library; if not, write to the Free Software
+!  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307
+!  USA
+
 #include "libsupermesh_debug.h"
 
 subroutine test_intersection_finder_completeness_1d() bind(c)
@@ -6,7 +58,7 @@ subroutine test_intersection_finder_completeness_1d() bind(c)
     & intersection_finder, advancing_front_intersection_finder, &
     & sort_intersection_finder, brute_force_intersection_finder
   use libsupermesh_interval_intersection, only : interval_buf_size, &
-    & intersect_intervals
+    & intersect_intervals, interval_size
   use libsupermesh_read_triangle, only : read_ele, read_node
   use libsupermesh_unittest_tools, only : report_test, operator(.fne.)
 
@@ -47,15 +99,6 @@ subroutine test_intersection_finder_completeness_1d() bind(c)
   deallocate(map_ba, positions_a, enlist_a, positions_b, enlist_b)
 
 contains
-
-  pure function interval_size(interval) result(size)
-    real, dimension(1, 2), intent(in) :: interval
-    
-    real :: size
-    
-    size = abs(interval(1, 2) - interval(1, 1))
-    
-  end function interval_size
 
   subroutine check_map_ba(name)
     character(len = *), intent(in) :: name
